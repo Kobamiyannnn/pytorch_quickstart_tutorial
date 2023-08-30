7c7b347c850cd9c9a1f2147e7c4a63c0d961885d
Kobamiyannnn <kobaprfm23@gmail.com>
2023-08-30 18:20:05 +0900

    [wip] "Creating Models"

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
 #####################
 # Working with data #
 #####################
@@ -31,6 +32,21 @@ train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
 for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of X [N, C, H, W]: {X.shape}")  # N: Batch size
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
+
+
+###################
+# Creating Models #
+###################
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
