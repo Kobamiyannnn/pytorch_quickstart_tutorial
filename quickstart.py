270a44bf0d0d685a377d67e37f213cc9a1a4520c
Kobamiyannnn <kobaprfm23@gmail.com>
2023-08-31 17:15:49 +0900

    [Done] Saving Models

--- quickstart.py
+++ quickstart.py
@@ -124,7 +124,7 @@ def test(dataloader, model, loss_fn):
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    corrent /= size
+    correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
@@ -134,3 +134,12 @@ for t in range(epochs):
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
 print("Done!")
+
+
+#################
+# Saving Models #
+#################
+print("\n" + "-" * 60 + "\n" + "< Saving Models >")
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
